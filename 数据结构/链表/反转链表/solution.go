018d5dee0bcc36251e3b8ded5b3316adc722d16a
zhanghaizhou <1078760300@qq.com>
2022-10-08 14:37:36 +0800

    反转链表

--- 数据结构/链表/反转链表/solution.go
+++ 数据结构/链表/反转链表/solution.go
@@ -6,5 +6,16 @@ type ListNode struct {
 }
 
 func reverseList(head *ListNode) *ListNode {
-	return nil
+	if head == nil {
+		return nil
+	}
+	//preHead := new(ListNode)
+	var preHead *ListNode
+	for head != nil {
+		next := head.Next
+		head.Next = preHead
+		preHead = head
+		head = next
+	}
+	return preHead
 }
