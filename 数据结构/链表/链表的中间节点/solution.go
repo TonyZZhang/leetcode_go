d185a422cbfce6fc29d31c4549ed1032422fdce6
zhanghaizhou <1078760300@qq.com>
2022-10-10 13:07:25 +0800

    链表的中间节点

--- 数据结构/链表/链表的中间节点/solution.go
+++ 数据结构/链表/链表的中间节点/solution.go
@@ -6,5 +6,20 @@ type ListNode struct {
 }
 
 func middleNode(head *ListNode) *ListNode {
-	return nil
+	if head == nil || head.Next == nil {
+		return head
+	}
+	fast := new(ListNode)
+	low := new(ListNode)
+	fast = head
+	low = head
+	for fast != nil {
+		if fast.Next != nil {
+			fast = fast.Next.Next
+			low = low.Next
+		} else {
+			return low
+		}
+	}
+	return low
 }
