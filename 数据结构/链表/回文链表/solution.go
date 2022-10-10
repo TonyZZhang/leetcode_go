73c2adaf921606ff5920c06016b9ac1cea4bf67c
zhanghaizhou <1078760300@qq.com>
2022-10-10 15:01:13 +0800

    回文链表

--- 数据结构/链表/回文链表/solution.go
+++ 数据结构/链表/回文链表/solution.go
@@ -5,6 +5,44 @@ type ListNode struct {
 	Next *ListNode
 }
 
+//1-2
+//1-2-1
+//1-2-2-1
 func isPalindrome(head *ListNode) bool {
-	return false
+	if head == nil {
+		return false
+	}
+	if head.Next == nil {
+		return true
+	}
+	newHead := new(ListNode)
+	newHead = head
+	var old []int
+	for head != nil {
+		old = append(old, head.Val)
+		head = head.Next
+	}
+	newList := reverseList(newHead)
+	for _, v := range old {
+		if v != newList.Val {
+			return false
+		} else {
+			newList = newList.Next
+		}
+	}
+	return true
+}
+
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	var preHead *ListNode
+	for head != nil {
+		next := head.Next
+		head.Next = preHead
+		preHead = head
+		head = next
+	}
+	return preHead
 }
