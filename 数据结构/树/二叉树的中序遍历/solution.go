35ded2a4fc411610ba68b5dfe1922f2b051cbdd9
zhanghaizhou <1078760300@qq.com>
2022-10-11 17:49:28 +0800

    二叉树的中序遍历

---
+++ 数据结构/树/二叉树的中序遍历/solution.go
@@ -0,0 +1,29 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+//        1
+//       / \
+//          2
+//         / \
+//        3
+
+func inorderTraversal(root *TreeNode) []int {
+	stack := []*TreeNode{}
+	var res []int
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		res = append(res, root.Val)
+		root = root.Right
+	}
+	return res
+}
---
+++ 数据结构/树/二叉树的中序遍历/solution_test.go
@@ -0,0 +1,14 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestName(t *testing.T) {
+	breakPoint := 0
+	breakPoint2 := 2
+	for true || false {
+		fmt.Println("--")
+	}
+}
