1dd7877d57c074ea04149d3c2e5ae7d3e4bc0e49
zhanghaizhou <1078760300@qq.com>
2022-10-12 15:24:27 +0800

    二叉树的前序遍历

--- 数据结构/树/二叉树的前序遍历/solution.go
+++ 数据结构/树/二叉树的前序遍历/solution.go
@@ -6,6 +6,30 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+//        1
+//       / \
+//          2
+//         / \
+//        3
 func preorderTraversal(root *TreeNode) []int {
-	return nil
+	if root == nil {
+		return []int{}
+	}
+	var res []int
+	var stack []*TreeNode
+	stack = append(stack, root)
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if node != nil {
+			res = append(res, node.Val)
+		}
+		if node.Right != nil {
+			stack = append(stack, node.Right)
+		}
+		if node.Left != nil {
+			stack = append(stack, node.Left)
+		}
+	}
+	return res
 }
