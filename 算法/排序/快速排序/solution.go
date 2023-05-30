0fb1795f84deaf3862c572c704a822042604e959
zhz <1078760300@qq.com>
2023-05-30 21:11:43 +0800

    快速排序

--- 算法/排序/快速排序/solution.go
+++ 算法/排序/快速排序/solution.go
@@ -33,3 +33,34 @@ func quickSort(nums []int) []int {
 	quickSort(nums[right+1:])
 	return nums
 }
+
+//2 1 3
+func quickSortV2(nums[]int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+
+	flag := nums[0]
+	left := 0
+	right := len(nums) - 1
+
+	for left < right {
+		for nums[right] > flag {
+			right--
+		}
+
+		for nums[left] < flag {
+			left++
+		}
+		temp := nums[left]
+		nums[left] = nums[right]
+		nums[right] = temp
+	}
+	nums[left] = flag
+	if left == 0 {
+		return nums
+	}
+	quickSortV2(nums[:left-1])
+	quickSortV2(nums[left+1:])
+	return nums
+}
