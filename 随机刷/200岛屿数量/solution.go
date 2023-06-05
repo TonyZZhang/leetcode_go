0bc3159411b4ce1677331733042b6a1eaae04db3
zhz <1078760300@qq.com>
2023-06-05 21:56:42 +0800

    200岛屿数量

--- 随机刷/200岛屿数量/solution.go
+++ 随机刷/200岛屿数量/solution.go
@@ -1,5 +1,39 @@
 package leetcode
-
+//Flood fill
 func numIslands(grid [][]byte) int {
+	res := 0
+	for i:=0; i< len(grid[0]); i++ {
+		for j:=0; j< len(grid); j++ {
+			if grid[i][j] == 1 {
+				fill(grid,i, j)
+				res++
+			}
+		}
+	}
 
+	return res
+}
+
+func fill(g [][]byte, i, j int) {
+	g[i][j] = 0
+	if i+1 < len(g) {
+		if g[i+1][j] == 1 {
+			fill(g, i+1, j)
+		}
+	}
+	if j+1 < len(g[i]) {
+		if g[i][j+1] == 1 {
+			fill(g, i, j+1)
+		}
+	}
+	if i-1 >= 0 {
+		if g[i-1][j] == 1 {
+			fill(g, i-1, j)
+		}
+	}
+	if j-1 >= 0 {
+		if g[i][j-1] == 1 {
+			fill(g, i, j-1)
+		}
+	}
 }
