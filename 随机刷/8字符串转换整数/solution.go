1a6c379470122b3f10d2319f91e4cd3598bec165
zhanghaizhou <1078760300@qq.com>
2023-06-16 13:04:00 +0800

    字符串转换整数

--- 随机刷/8字符串转换整数/solution.go
+++ 随机刷/8字符串转换整数/solution.go
@@ -1,5 +1,14 @@
 package leetcode
 
 func myAtoi(s string) int {
-
+	if len(s) == 0 {
+		return -1
+	}
+	res := 0
+	tenNum := 1
+	for i := len(s)-1; i >= 0 ; i-- {
+		res = res + tenNum* int((s[i]-'0'))
+		tenNum = tenNum * 10
+	}
+	return res
 }
