24e29ddc5e1355abce6171318876e32ff0e38384
zhanghaizhou <1078760300@qq.com>
2023-06-17 14:19:27 +0800

    322零钱兑换

--- 随机刷/322零钱兑换/solution.go
+++ 随机刷/322零钱兑换/solution.go
@@ -2,28 +2,38 @@ package leetcode
 
 import (
 	"sort"
-	"math"
 )
 //动态规划问题
-// dp[i]代表兑换面额为i的最大硬币数
+// dp[i]代表兑换面额为i的最小硬币数
 // dp[i] = dp[i-coin] + 1
+// 2
 // [1,2,4]
+// 默认dp[i]=-1 也就是没有能兑换的
 
 func coinChange(coins []int, amount int) int {
 	sort.Ints(coins)
-	dp := make([]int, amount)
+	dp := make([]int, amount+1)
 	dp[0] = 0
-	for i:=0; i<=amount; i++ {
+	for i:=1; i < len(dp); i++ {
+		dp[i] = -1
+	}
+	for i:=1; i<=amount; i++ {
 		for k := 0; k < len(coins); k++ {
 			temp := 0
 			if i-coins[k] >=0 {
+				if dp[i-coins[k]] == -1 {
+					continue
+				}
 				temp = dp[i-coins[k]] + 1
+			} else {
+				break
 			}
-			if temp > dp[i] {
+			if temp < dp[i] {
+				dp[i] = temp
+			} else if dp[i] == -1 && temp > 0{
 				dp[i] = temp
 			}
 		}
 	}
-	sort.Ints(dp)
-	return dp[len(dp)-1]
+	return dp[amount]
 }
