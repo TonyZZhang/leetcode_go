e3ec8602544badf23a4d7de47586b388a6ebb9b4
zhz <1078760300@qq.com>
2023-06-19 20:33:26 +0800

    78子集

--- 随机刷/78子集/solution.go
+++ 随机刷/78子集/solution.go
@@ -1,1 +1,27 @@
-package _8子集
+package leetcode
+
+func subsets(nums []int) [][]int {
+	if len(nums) == 0 {
+		return [][]int{}
+	}
+
+	dp := make([][]int, 0)
+	s := nums[0]
+	dp = append(dp, []int{})
+	dp = append(dp, []int{s})
+
+	for i := 1; i < len(nums); i++ {
+		curLen := len(dp)
+		for k := 0; k < curLen; k++ {
+			item := dp[k]
+			newItem := make([]int,0)
+			for v := 0; v < len(item); v++ {
+				newItem = append(newItem, item[v])
+			}
+			newItem = append(newItem, nums[i])
+			dp = append(dp, newItem)
+		}
+	}
+	return dp
+}
+
