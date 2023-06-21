867ff7dbdbc5a6362c227d181aba4e5c59273701
zhanghaizhou <1078760300@qq.com>
2023-06-21 17:02:06 +0800

    对称二叉树

--- 随机刷/101对称二叉树/solution.go
+++ 随机刷/101对称二叉树/solution.go
@@ -8,17 +8,42 @@ type TreeNode struct {
 
 
 func isSymmetric(root *TreeNode) bool {
+	que := make([]*TreeNode, 0)
 	if root == nil {
 		return true
 	}
-	if root.Left == nil && root.Right == nil {
-		return true
+	if root.Left != nil {
+		que = append(que, root.Left)
 	}
-	if root.Left != nil && root.Right == nil {
+	if root.Right != nil {
+		que = append(que, root.Right)
+	}
+	if len(que)%2 != 0 {
 		return false
 	}
-	if root.Left == nil && root.Right != nil {
-		return false
+
+	curQueLen := len(que)
+	for i:= 0; i<curQueLen; i++{
+		if que[i].Left != nil {
+			que = append(que, que[i].Left)
+		}
+		if que[i].Right != nil {
+			que = append(que, que[i].Right)
+		}
+		if i == curQueLen-1 {
+			curQueLen = len(que)
+			l := curQueLen-1-i
+			if l%2 != 0 {
+				return false
+			}
+			for k:= i+1; k <= i+(l/2); k++ {
+				if que[k].Val != que[curQueLen-1-k].Val {
+					return false
+				}
+			}
+		}
 	}
-	return isSymmetric(root.Left) && isSymmetric(root.Right)
+
+	return true
 }
+
