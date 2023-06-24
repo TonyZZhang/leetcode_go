65dcacfd4f24360a7701413d655be5feaed6770f
zhz <1078760300@qq.com>
2023-06-24 23:38:04 +0800

    迭代未AC，递归AC

--- 随机刷/101对称二叉树/solution.go
+++ 随机刷/101对称二叉树/solution.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "fmt"
+
 type TreeNode struct {
 	Val int
 	Left *TreeNode
@@ -37,13 +39,31 @@ func isSymmetric(root *TreeNode) bool {
 				return false
 			}
 			for k:= i+1; k <= i+(l/2); k++ {
-				if que[k].Val != que[curQueLen-1-k].Val {
+				if que[k].Val != que[curQueLen-1-(k-(i+1))].Val {
 					return false
 				}
 			}
 		}
 	}
-
 	return true
 }
 
+func isSymmetricV2(root *TreeNode) bool {
+	return isMrror(root.Left, root.Right)
+}
+
+func isMrror(left *TreeNode, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+
+	if left == nil || right == nil {
+		return false
+	}
+
+	if left.Val != right.Val {
+		return false
+	}
+
+	return isMrror(left.Left, right.Right) && isMrror(left.Right, right.Left)
+}
