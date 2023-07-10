fc742a774853bf2c84933936a9bfcb527fcbafe8
zhanghaizhou <1078760300@qq.com>
2023-07-10 14:17:45 +0800

    14最长公共前缀

--- 随机刷/14最长公共前缀/solution.go
+++ 随机刷/14最长公共前缀/solution.go
@@ -1,5 +1,23 @@
 package leetcode
 
-func findPreStr([]string) string {
-	return ""
+import "strings"
+
+//输入：strs = ["flower","flow","flight"]
+//输出："fl"
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	r := strs[0]
+	for i := 1; i < len(strs); i++ {
+		for !strings.HasPrefix(strs[i], r) {
+			r = r[:len(r)-1]
+			if len(r) == 0 {
+				return ""
+			}
+		}
+	}
+
+	return r
 }
