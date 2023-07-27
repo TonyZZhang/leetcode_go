116341281cd3327d4cfac1cfa1a0b1d15ccc9bc9
zhanghaizhou <1078760300@qq.com>
2023-07-27 14:36:59 +0800

    update 121

--- Hot100/Array/121/solution.go
+++ Hot100/Array/121/solution.go
@@ -1,25 +1,26 @@
 package leetcode
 
-dp[i] = dp[i-1] + prices[i]-prices[i-1]
+//dp[i] = dp[i-1] + prices[i]-prices[i-1]
 
 func maxProfit(prices []int) int {
 	var res int
-	dp := make([]int, len(prices)+1)
-	dp[0] = 0
-	for i:=1; i<len(prices);i++ {
-		if prices[i]-prices[i-1] > 0 {
-			dp[i] = dp[i-1]+prices[i]-prices[i-1]
-			res = max(res, dp[i])
-		}else {
-			dp[i] = dp[i-1]
-			res = max(res, dp[i])
-		}
+	minP := prices[0]
+	for i := 0; i < len(prices); i++ {
+		minP = min(prices[i], minP)
+		res = max(res, prices[i]-minP)
 	}
 	return res
 }
 
 func max(a, b int) int {
 	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
 		return a
 	}
 	return b
